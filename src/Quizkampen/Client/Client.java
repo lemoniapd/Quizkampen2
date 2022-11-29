73021d43c3073b9881d50748325ca154417b2ed1
lemoniapd <lemonia.pd@gmail.com>
2022-11-29 12:54:43 +0100

    Changes by User

--- src/Quizkampen/Client/Client.java
+++ src/Quizkampen/Client/Client.java
@@ -1,4 +1,21 @@
 package Quizkampen.Client;
 
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 public class Client {
+
+    private int port = 22222;
+
+    public Client(InetAddress ip) {
+    }
+
+    public static void main(String[] args) throws UnknownHostException {
+        InetAddress ip = InetAddress.getLocalHost();
+        new Client(ip);
+    }
 }
+
